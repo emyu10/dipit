f6e0113727b22973536dec793569ca497b7cff87
Mohamed Usman <emyu10@gmail.com>
2018-02-01 22:44:58 +0500

    some javascript for loading nav links

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,14 +2,28 @@ var app = new Object();
 
 window.addEventListener('load', function() {
     app.navbar = document.getElementById('navbar');
-    // loadNavLinks();
+    app.sidebar = document.getElementById('sidebar');
+    app.sidebarLinks = document.getElementById('sidebar-links');
+    app.sidebarContent = document.getElementById('sidebar-content');
+    app.loadedModule = '';
+    loadNavLinks();
 }, false);
 
 function loadNavLinks() {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function(event) {
         console.log(event);
-        app.navbar.innerHTML = 'Navbar loaded';
+        var jr = JSON.parse(event.target.responseText);
+        for (i = 0; i < jr.length; i++) {
+            var navLink = document.createElement('a');
+            navLink.innerText = jr[i].title;
+            navLink.href = jr[i].link;
+            if (app.navbar.innerText == 'loading...') {
+                app.navbar.innerText = '';
+            }
+            app.navbar.appendChild(navLink);
+        }
+
     }, false);
     xhr.addEventListener('error', function(event) {
         app.navbar.innerHTML = 'Error loading the navbar';
